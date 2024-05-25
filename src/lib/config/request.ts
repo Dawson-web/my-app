ac65f588c4581d8dfe02cd179ab7ef8daf1e7232
DawSon <3096567831@qq.com>
2024-05-25 23:17:21 +0800

    fix

--- src/lib/config/request.ts
+++ src/lib/config/request.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const request = axios.create({
-  baseURL: "https://127.0.0.1:7890",
+  baseURL: "https://127.0.0.1:3000",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
