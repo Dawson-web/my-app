ac77ab0df56f99db686459db7f114cb99f2f1872
DawSon <3096567831@qq.com>
2024-05-25 22:53:32 +0800

    fix

--- src/lib/config/db.ts
+++ src/lib/config/db.ts
@@ -1,6 +1,16 @@
 export const ConnectDB = async () => {
   const mongoose = require("mongoose");
-  const url = process.env.MONGODB_URI;
-  await mongoose.connect(url);
+  const uri = process.env.MONGODB_URI;
+  await mongoose.connect(uri);
   console.log("Connected to MongoDB");
+  // var MongoClient = require("mongodb").MongoClient;
+  // var uri =
+  //   "mongodb://Dawson:3311498Dawson.@ac-g23nobp-shard-00-00.t3efd4t.mongodb.net:27017,ac-g23nobp-shard-00-01.t3efd4t.mongodb.net:27017,ac-g23nobp-shard-00-02.t3efd4t.mongodb.net:27017/?ssl=true&replicaSet=atlas-v42p3y-shard-0&authSource=admin&retryWrites=true&w=majority&appName=my-app";
+  // MongoClient.connect(uri, function (err, client) {
+  //   const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  // console.log("Connected to MongoDB");
+
+  // client.close();
+  // });
 };
