ec913c51c2b4284b288c79342d1f81821796708d
DawSon <3096567831@qq.com>
2024-05-25 15:22:42 +0800

    update:request timeout

--- src/lib/config/request.ts
+++ src/lib/config/request.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const request = axios.create({
   baseURL: "https://127.0.0.1:3000",
-  timeout: 5000,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
