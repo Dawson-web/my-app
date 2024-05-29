eba8ff089f10ed737649abca075a6476aafb6588
DawSon <3096567831@qq.com>
2024-05-30 00:15:22 +0800

    fix: type problem

--- src/components/client/RenderComponent.tsx
+++ src/components/client/RenderComponent.tsx
@@ -1,7 +1,7 @@
 "use client";
 import ArticleUpload from "@/components/client/ArticleUpload";
 import ArticleDelete from "./ArticleDelete";
-export const RenderComponent = (props) => {
+export const RenderComponent = (props: any) => {
   const { component } = props; // Assuming the component prop is a string that represents the component to render
   switch (component) {
     case "ArticleUpload":
