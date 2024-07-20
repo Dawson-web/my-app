cfd9c34d7d27cee7c3edf335dbed161d5d882971
DawSon <3096567831@qq.com>
2024-07-20 10:19:06 +0800

    feat: add x/y layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,15 +1,31 @@
+"use client";
 import Menu from "@/components/client/menu";
 import { Toaster } from "@/components/ui/sonner";
+import clsx from "clsx";
+import { useState } from "react";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [layout, setLayout] = useState<string>(
+    localStorage.getItem("layout") || "horizontal"
+  );
   return (
-    <main className="flex flex-col flex-1 sm:flex-row">
-      <Menu />
-      <section className="w-full flex flex-col items-center h-screen bg-gray-200 dark:bg-gray-900">
+    <main
+      className={clsx("flex h-screen flex-1 sm:flex-row overflow-y-hidden", {
+        "sm:flex-col": layout === "vertical",
+        "flex-row": layout === "horizontal",
+        "flex-col": layout === "vertical",
+      })}
+    >
+      <Menu layout={layout} setLayout={setLayout} />
+      <section
+        className={clsx(
+          "w-full flex flex-col items-center h-screen bg-gray-200 dark:bg-gray-900"
+        )}
+      >
         {children}
       </section>
       <Toaster />
--- src/components/client/menu.tsx
+++ src/components/client/menu.tsx
@@ -10,22 +10,76 @@ import {
 import Link from "next/link";
 import clsx from "clsx";
 import { useState } from "react";
-import { Button } from "../ui/button";
 
-export default function Menu() {
+export default function Menu(props: any) {
   const [option, setOption] = useState<string>("blog");
   const [theme, setTheme] = useState<boolean>(true);
+
+  const { layout, setLayout } = props;
   function Theme() {
     return localStorage.theme === "dark";
   }
+
   return (
-    <NavigationMenu className="shadow-lg">
-      <NavigationMenuList className="flex sm:flex-col  ">
+    <NavigationMenu
+      className={clsx("shadow-lg ", {
+        "sm:mt-[20px]": layout === "horizontal",
+      })}
+    >
+      <NavigationMenuList
+        className={clsx("flex ", {
+          "sm:flex-row": layout === "vertical",
+          "sm:flex-col": layout === "horizontal",
+          "flex-col": layout === "horizontal",
+          "flex-row": layout === "vertical",
+        })}
+      >
+        <NavigationMenuItem onClick={() => setOption("blog")}>
+          <div className="flex p-2 gap-2">
+            <div className="w-[50%] flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+                className={clsx({
+                  "text-blue-600 ": layout == "horizontal",
+                })}
+                onClick={() => {
+                  setLayout("horizontal");
+                  localStorage.setItem("layout", "horizontal");
+                }}
+              >
+                <path d="M0 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3zm5-1v12h9a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H5zM4 2H2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h2V2z" />
+              </svg>
+            </div>
+            <div className="w-[50%] flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+                className={clsx({
+                  "text-blue-600": layout == "vertical",
+                })}
+                onClick={() => {
+                  setLayout("vertical");
+                  localStorage.setItem("layout", "vertical");
+                }}
+              >
+                <path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm13 2v2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zM2 14a1 1 0 0 1-1-1V6h14v7a1 1 0 0 1-1 1H2z" />
+              </svg>
+            </div>
+          </div>
+        </NavigationMenuItem>
         <NavigationMenuItem onClick={() => setOption("blog")}>
           <Link href="/dashboard/blog" legacyBehavior passHref>
             <NavigationMenuLink
               className={clsx(navigationMenuTriggerStyle(), {
-                "border-b-2 border-blue-600": option == "blog",
+                "border-b-2 border-blue-600 bg-blue-100 dark:bg-gray-800":
+                  option == "blog",
               })}
             >
               <svg
@@ -51,7 +105,8 @@ export default function Menu() {
           <Link href="/dashboard/control" legacyBehavior passHref>
             <NavigationMenuLink
               className={clsx(navigationMenuTriggerStyle(), {
-                "border-b-2 border-blue-600": option == "control",
+                "border-b-2 border-blue-600 bg-blue-100 dark:bg-gray-800":
+                  option == "control",
               })}
             >
               <svg
